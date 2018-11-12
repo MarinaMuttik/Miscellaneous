4e30930e549bb8019d1b422df5948424413c283d
Marina Muttik <mmuttik@live.co.uk>
2018-11-12 18:24:44 +0000

    updated with style changes

--- each_count.rb
+++ each_count.rb
@@ -1,42 +1,34 @@
 class Array
+  def each_count(method = nil, *args)
 
-  def each_count(method = nil, *params)
-
-    if method == nil && block_given? == false
-      hash = self.reduce(Hash.new(0)) { |key, value| key[value] += 1; key }
-      hash
-    elsif method != nil && block_given? == false
-      method_hash = self.map do |element|
-        if element.respond_to?(method)
-          element.send(method, *params)
-        else
-          return "Error: does not respond to provided method"
+    if method.nil? && block_given? == false
+      each_with_object(Hash.new(0)) { |key, value| value[key] += 1 }
+    elsif method && block_given? == false
+      map do |element|
+        return 'Error: does not respond to provided method' if element.respond_to?(method) == false
+        element.send(method, *args)
         end
-      end
-      method_hash.each_count
-    elsif method == nil && block_given?
-      block_hash = self.map { |element| yield element } .each_count
-      block_hash
-    elsif method != nil && block_given?
-      "Error: both method and block provided!"
+      .each_count
+    elsif method.nil? && block_given?
+      map { |element| yield element } .each_count
+    elsif method && block_given?
+      'Error: both method and block provided!'
     end
-
   end
-
 end
 
-cities = ["Melbourne", "Dallas", "Taipei", "Toronto", "Dallas", "Kathmandu"]
+cities = ['Melbourne', 'Dallas', 'Taipei', 'Toronto', 'Dallas', 'Kathmandu']
 
 p cities.each_count
-# => {"Melbourne"=>1, "Dallas"=>2, "Taipei"=>1, "Toronto"=>1, "Kathmandu"=>1}
+# => {'Melbourne'=>1, 'Dallas'=>2, 'Taipei'=>1, 'Toronto'=>1, 'Kathmandu'=>1}
 
 p cities.each_count(:length)
 # => {9=>2, 6=>3, 7=>1}
 
 p cities.each_count(:gsub, /[aeiou]/, 'x')
-# => {"Mxlbxxrnx"=>1, "Dxllxs"=>2, "Txxpxx"=>1, "Txrxntx"=>1, "Kxthmxndx"=>1}
+# => {'Mxlbxxrnx'=>1, 'Dxllxs'=>2, 'Txxpxx'=>1, 'Txrxntx'=>1, 'Kxthmxndx'=>1}
 
-p cities.each_count {|city| city.length % 3 == 0}
+p cities.each_count { |city| (city.length % 3).zero? }
 # => {true=>5, false=>1}
 
 p cities.each_count(:values)
