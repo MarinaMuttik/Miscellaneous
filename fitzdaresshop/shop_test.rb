33ea63f7a1a655afb66e9987a7a26c201c6f1b2c
Marina Muttik <mmuttik@live.co.uk>
2019-01-24 23:58:20 +0000

    updated products to global variables for use

--- fitzdaresshop/shop_test.rb
+++ fitzdaresshop/shop_test.rb
@@ -2,11 +2,11 @@ require_relative './checkout'
 
 # Test 1
 test1 = Checkout.new
-test1.scan(@gr1)
-test1.scan(@sr1)
-test1.scan(@gr1)
-test1.scan(@gr1)
-test1.scan(@cf1)
+test1.scan($gr1)
+test1.scan($sr1)
+test1.scan($gr1)
+test1.scan($gr1)
+test1.scan($cf1)
 price = test1.total
 expected = '£22.45'
 if price == expected
@@ -17,8 +17,8 @@ end
 
 # Test 2
 test2 = Checkout.new
-test2.scan(@gr1)
-test2.scan(@gr1)
+test2.scan($gr1)
+test2.scan($gr1)
 price = test2.total
 expected = '£3.11'
 if price == expected
@@ -29,10 +29,10 @@ end
 
 # Test 3
 test3 = Checkout.new
-test3.scan(@sr1)
-test3.scan(@sr1)
-test3.scan(@gr1)
-test3.scan(@sr1)
+test3.scan($sr1)
+test3.scan($sr1)
+test3.scan($gr1)
+test3.scan($sr1)
 price = test3.total
 expected = '£16.61'
 if price == expected
