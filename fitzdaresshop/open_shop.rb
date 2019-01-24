4017945f4ecd567466aa6c9d54ba43e4379b0ef1
Marina Muttik <mmuttik@live.co.uk>
2019-01-24 23:58:09 +0000

    updated products to global variables for use

--- fitzdaresshop/open_shop.rb
+++ fitzdaresshop/open_shop.rb
@@ -1,5 +1,6 @@
 require_relative './product'
+require_relative './offer'
 
-@gr1 = Product.new('Green Tea', 'GR1', 3.11)
-@sr1 = Product.new('Strawberries', 'SR2', 5.00)
-@cf1 = Product.new('Coffee', 'CF1', 11.23)
+$gr1 = Product.new('Green Tea', 'GR1', 3.11)
+$sr1 = Product.new('Strawberries', 'SR2', 5.00)
+$cf1 = Product.new('Coffee', 'CF1', 11.23)
