095c3225d1e4d7826590630a1295b9fa1162aadb
Marina Muttik <mmuttik@live.co.uk>
2019-01-25 21:59:06 +0000

    added passing method for second test

--- rubytest/int_to_words.rb
+++ rubytest/int_to_words.rb
@@ -7,7 +7,17 @@ $num_to_name = {
   6 => 'six',
   7 => 'seven',
   8 => 'eight',
-  9 => 'nine'
+  9 => 'nine',
+  10 => 'ten',
+  11 => 'eleven',
+  12 => 'twelve',
+  13 => 'thirteen',
+  14 => 'fourteen',
+  15 => 'fifteen',
+  16 => 'sixteen',
+  17 => 'seventeen',
+  18 => 'eighteen',
+  19 => 'nineteen'
 }
 
 # Method to convert integer input to words
