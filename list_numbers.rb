482e24edf018ad0e7d8c76b209a6af5cd543696b
Marina Muttik <mmuttik@live.co.uk>
2019-02-26 21:47:05 +0000

    added method to fail test

--- list_numbers.rb
+++ list_numbers.rb
@@ -4,7 +4,11 @@ expected = "-6,-3-1,3-5,7-11,14,15,17-20"
 result = solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
 
 if expected == result
-  "The expected result of #{result} was achieved"
+  "Test passed: The expected result of #{result} was achieved"
 else
   "Test failed: expected #{expected}, received #{result}"
+end
+
+def solution(list)
+  list
 end
