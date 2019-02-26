27e969f8333aad6f201a4322ec7a56cbbceea38f
Marina Muttik <mmuttik@live.co.uk>
2019-02-26 22:07:15 +0000

    added second failing test

--- list_numbers.rb
+++ list_numbers.rb
@@ -20,11 +20,26 @@ end
 
 # Testing
 
-expected = '-6,-3-1,3-5,7-11,14,15,17-20'
-result = solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+# Test 1 - consecutive numbers
 
-if expected == result
-  p "Test passed: The expected result of #{result} was achieved"
+expected1 = '-6,-3-1,3-5,7-11,14,15,17-20'
+result1 = solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+
+p 'Test 1 - consecutive numbers:'
+if expected1 == result1
+  p "Test passed: The expected result of #{result1} was achieved"
 else
-  p "Test failed: expected #{expected}, received #{result}"
+  p "Test failed: expected #{expected1}, received #{result1}"
+end
+
+# Test 2 - non-consecutive numbers
+
+expected2 = '-4--1,3,4,6,8,11-13'
+result2 = solution([-2, -3, -1, -4, 3, 4, 8, 6, 11, 13, 12])
+
+p 'Test 2 - non-consecutive numbers:'
+if expected2 == result2
+  p "Test passed: The expected result of #{result2} was achieved"
+else
+  p "Test failed: expected #{expected2}, received #{result2}"
 end
