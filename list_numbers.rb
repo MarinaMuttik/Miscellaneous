0bd2ac19ebc309df1518a29e4bbf399cfb7c5251
Marina Muttik <mmuttik@live.co.uk>
2019-02-26 21:49:03 +0000

    corrected order of testing to work with method and printing results

--- list_numbers.rb
+++ list_numbers.rb
@@ -1,14 +1,16 @@
+# Body
+
+def solution(list)
+  'fail'
+end
+
 # Testing
 
-expected = "-6,-3-1,3-5,7-11,14,15,17-20"
+expected = '-6,-3-1,3-5,7-11,14,15,17-20'
 result = solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
 
 if expected == result
-  "Test passed: The expected result of #{result} was achieved"
+  p "Test passed: The expected result of #{result} was achieved"
 else
-  "Test failed: expected #{expected}, received #{result}"
-end
-
-def solution(list)
-  list
+  p "Test failed: expected #{expected}, received #{result}"
 end
